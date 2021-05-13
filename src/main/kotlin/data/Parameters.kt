74b24326b5372cc0b960921a7a03b226598e45a7
Christopher Montez <yduaskme@tamu.edu>
2021-05-13 16:11:11 -0500

    Added the Parameters singleton that contains relevant information for the instance and solver

--- src/main/kotlin/data/Parameters.kt
+++ src/main/kotlin/data/Parameters.kt
@@ -1,6 +1,52 @@
 package data
 
-class Parameters
-{
+/*
+    Making an object called Parameters. In Kotlin, an object is a singleton (i.e., a single instantiation of a class).
+    By creating Parameters using "object", we are guaranteed that Parameters is thread-safe.
+ */
 
+
+/**
+ * Singleton to hold all the parameters for the solver and the instance to be solved.
+ */
+object Parameters
+{
+    /**
+     * File name of the instance including the .txt extension.
+     */
+    var instanceName: String = "p2.2.a.txt"
+        private set
+
+    /**
+     * File path of the local folder containing the .txt for the problem instance.
+     */
+    var instancePath: String = "./data/Set_21_234/"
+        private set
+
+    /**
+     * Maximum time (in seconds) for the solver.
+     */
+    var timeLimitInSeconds: Int = 600
+        private set
+
+    /**
+     * Tolerance used for comparing double values.
+     */
+    const val eps = 1e-5
+
+    /**
+     * Number of bits of the operating system.
+     */
+    const val numBits = 64
+
+    fun initialize(
+        instanceName: String,
+        instancePath: String,
+        timeLimitInSeconds: Int
+    )
+    {
+        Parameters.instanceName = instanceName
+        Parameters.instancePath = instancePath
+        Parameters.timeLimitInSeconds = timeLimitInSeconds
+    }
 }
