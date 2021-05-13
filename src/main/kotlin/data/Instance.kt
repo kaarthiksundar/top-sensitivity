2de07661d4ae005ad7be0fbb1b09db1313de4f9f
Christopher Montez <yduaskme@tamu.edu>
2021-05-13 14:58:31 -0500

    Added documentation to instance class

--- src/main/kotlin/data/Instance.kt
+++ src/main/kotlin/data/Instance.kt
@@ -4,6 +4,20 @@ package data
  TODO: Determine what parameters need to be stored for each instance
 */
 
+/**
+ * Represents a problem instance of the Euclidean team orienteering problem.
+ *
+ * @param name String: Name of file including the .txt extension
+ * @param path String: Local path of the folder containing [name]
+ * @param graph Object representing the directed graph for the problem instance
+ * @param numVertices Int: Number of vertices in the graph
+ * @param numVehicles Int: Number of vehicles used in the team orienteering problem
+ * @param source Int: Index of the source node in the graph
+ * @param destination Int: Index of the destination node in the graph
+ * @param prizes List<Double>: List of prizes at each node in the graph
+ * @param budget Double: Maximum amount of time/length allotted for each vehicle.
+ */
+
 data class Instance(
     val name: String,
     val path: String,
