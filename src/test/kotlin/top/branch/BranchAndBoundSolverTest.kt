015a53b9b3df6b7c0266bffe0847971238f7ffe1
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2021-06-04 11:53:43 -0700

    test branch and bound with 2 knapsack models

--- src/test/kotlin/top/branch/BranchAndBoundSolverTest.kt
+++ src/test/kotlin/top/branch/BranchAndBoundSolverTest.kt
@@ -6,42 +6,33 @@ import ilog.cplex.IloCplex
 import top.Util
 import kotlin.test.*
 
-/**
- * These tests will check whether the branch and bound API solves and calculates solution stats
- * for the following model.
- *
- *     Maximize   24 x1 + 2 x2 + 20 x3 + 4 x4
- *     subject to 8 x1 + x2 + 5x3 + 4x4 <= 9
- *     x1,x2,x3,x4 binary
- *
- *     The solution for this problem is 26 with x1=1, x2=1.
- */
 class BranchAndBoundSolverTest {
     @Test
-    fun `model should be solved correctly in parallel mode`() {
-        val solvers = List(5) { CplexSolver() }
-        val solution = BranchAndBoundSolver(solvers).solve(::branch)
-        assertNotNull(solution)
-        assertEquals(26.0, solution.objective, Util.EPS)
-        assertTrue(solution.maxParallelSolves > 1)
-        assertNotEquals(1, solution.maxParallelSolves)
-    }
+    fun `binary problems should be solved correctly in parallel and sequential modes`() {
+        for (useModel1 in listOf(true, false)) {
+            for (numSolvers in listOf(1, 5)) {
+                val solvers = List(numSolvers) { CplexSolver(useModel1) }
+                val solution = BranchAndBoundSolver(solvers).solve(::branch)
+                assertNotNull(solution)
+                assertTrue(solution.numCreatedNodes > 1)
+                assertTrue(solution.numFeasibleNodes <= solution.numCreatedNodes)
 
-    @Test
-    fun `model should be solved correctly in sequential mode`() {
-        val solvers = listOf(CplexSolver())
-        val solution = BranchAndBoundSolver(solvers).solve(::branch)
-        assertNotNull(solution)
-        assertEquals(26.0, solution.objective, Util.EPS)
-        assertEquals(1, solution.maxParallelSolves)
+                val expectedObjective = CplexSolver(useModel1).getMIPObjective()
+                assertEquals(expectedObjective, solution.objective, Util.EPS)
+
+                if (numSolvers > 1)
+                    assertTrue(solution.maxParallelSolves > 1)
+                else
+                    assertEquals(1, solution.maxParallelSolves)
+            }
+        }
     }
 }
 
-private class CplexSolver : ISolver {
+private class CplexSolver(private val model1: Boolean = true) : ISolver {
     private val cplex = IloCplex()
 
-    private fun buildSimpleModel(x: Array<IloNumVar>) {
-        cplex.clearModel()
+    private fun buildModel1(x: Array<IloNumVar>) {
         val objCoefficients = doubleArrayOf(24.0, 2.0, 20.0, 4.0)
         cplex.addMaximize(cplex.scalProd(objCoefficients, x))
 
@@ -49,10 +40,34 @@ private class CplexSolver : ISolver {
         cplex.addLe(cplex.scalProd(constraintCoefficients, x), 9.0, "knapsack")
     }
 
+    private fun buildModel2(x: Array<IloNumVar>) {
+        val objCoefficients = doubleArrayOf(16.0, 22.0, 12.0, 8.0, 11.0, 19.0)
+        cplex.addMaximize(cplex.scalProd(objCoefficients, x))
+
+        val constraintCoefficients = doubleArrayOf(5.0, 7.0, 4.0, 3.0, 4.0, 6.0)
+        cplex.addLe(cplex.scalProd(constraintCoefficients, x), 14.0, "knapsack")
+    }
+
+    fun getMIPObjective(): Double {
+        cplex.clearModel()
+        val x = cplex.numVarArray(if (model1) 4 else 6, 0.0, 1.0)
+        if (model1)
+            buildModel1(x)
+        else
+            buildModel2(x)
+        cplex.add(cplex.conversion(x, IloNumVarType.Bool))
+        cplex.setOut(null)
+        cplex.solve()
+        return cplex.objValue
+    }
+
     override fun solve(unsolvedNode: Node): Node {
         cplex.clearModel()
-        val x = cplex.numVarArray(4, 0.0, 1.0)
-        buildSimpleModel(x)
+        val x = cplex.numVarArray(if (model1) 4 else 6, 0.0, 1.0)
+        if (model1)
+            buildModel1(x)
+        else
+            buildModel2(x)
 
         for ((index, value) in unsolvedNode.restrictions) {
             cplex.addEq(x[index], value.toDouble(), "restriction_x$index")
