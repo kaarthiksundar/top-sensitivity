b1fd3ecc2e9d99749a245a10e3602c8d6d644192
Christopher Montez <yduaskme@tamu.edu>
2021-07-06 16:30:47 -0500

    Small speed improvements in PricingProblem
    Removed redundant variable storage.
    Modified if-statements to be slightly faster.

--- src/main/kotlin/top/solver/PricingProblem.kt
+++ src/main/kotlin/top/solver/PricingProblem.kt
@@ -255,7 +255,6 @@ class PricingProblem(
             return
 
         // Admissible elementary route found
-        //val joinedPath = forwardState.getPartialPath().asReversed() + backwardState.getPartialPath()
 
         if (!(forwardState.hasCycle || backwardState.hasCycle) && !forwardState.hasCommonGeneralVisits(backwardState)) {
             // Path is elementary
@@ -327,7 +326,6 @@ class PricingProblem(
 
         // Iterating over all possible extensions to neighboring vertices
         for (e in graph.outgoingEdgesOf(currentVertex)) {
-            val edgeLength = graph.getEdgeWeight(e)
             val newVertex = graph.getEdgeTarget(e)
 
             // Don't extend to critical vertices more than once
@@ -339,6 +337,7 @@ class PricingProblem(
                 continue
 
             // Checking if an extension is feasible
+            val edgeLength = graph.getEdgeWeight(e)
             val extension = extendIfFeasible(currentState, newVertex, edgeLength) ?: continue
 
             // Extension is feasible. Update unprocessed forward states
@@ -356,7 +355,6 @@ class PricingProblem(
 
         // Iterating over all possible extensions using incoming edges
         for (e in graph.incomingEdgesOf(currentVertex)) {
-            val edgeLength = graph.getEdgeWeight(e)
             val newVertex = graph.getEdgeSource(e)
 
             // Don't extend to critical vertices more than once
@@ -368,6 +366,7 @@ class PricingProblem(
                 continue
 
             // Checking if an extension is feasible
+            val edgeLength = graph.getEdgeWeight(e)
             val extension = extendIfFeasible(currentState, newVertex, edgeLength) ?: continue
 
             // Extension is feasible. Update unprocessed backward states
@@ -381,25 +380,21 @@ class PricingProblem(
      */
     private fun extendIfFeasible(currentState: State, newVertex: Int, edgeLength: Double) : State? {
 
-        // Length of partial path after extension
-        val newPathLength = currentState.length + edgeLength
-
         // Checking if new path is elementary or if the path length exceeds the budget
-        if (currentState.usedCriticalVertex(newVertex, parameters) || newPathLength > budget)
+        if (currentState.usedCriticalVertex(newVertex, parameters) ||
+            currentState.length + edgeLength > budget)
             return null
 
         // Extension is feasible
-        var edgeCost = vertexReducedCosts[newVertex]
-        edgeCost +=
-            if (currentState.isForward) {edgeDuals[currentState.vertex][newVertex]}
-            else {edgeDuals[newVertex][currentState.vertex]}
-        val newVertexScore = instance.scores[newVertex]
+        val edgeCost =
+            if (currentState.isForward) vertexReducedCosts[newVertex] + edgeDuals[currentState.vertex][newVertex]
+            else vertexReducedCosts[newVertex] + edgeDuals[newVertex][currentState.vertex]
 
         return currentState.extend(
             newVertex = newVertex,
             edgeCost = edgeCost,
             edgeLength = edgeLength,
-            newVertexScore = newVertexScore,
+            newVertexScore = instance.scores[newVertex],
             isCritical = isCritical[newVertex],
             parameters = parameters
         )
