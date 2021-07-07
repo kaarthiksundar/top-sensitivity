305d65f3c1631722ab36cccfa46ccc4f8b672c1e
Christopher Montez <yduaskme@tamu.edu>
2021-07-07 12:05:00 -0500

    Made TOPNode data class.
    Node class to be used in branch-and-bound for TOP.
    
    Will need to add a few more properties in the future.

---
+++ src/main/kotlin/top/solver/TOPNode.kt
@@ -0,0 +1,29 @@
+package top.solver
+
+import branchandbound.api.INode
+
+data class TOPNode(
+    override val id: Long,
+    override val parentLpObjective: Double = Double.MAX_VALUE,
+    override val lpFeasible: Boolean = false,
+    override val lpIntegral: Boolean = false,
+    override val lpObjective: Double = 0.0
+) : INode {
+
+    override val mipObjective : Double? = null
+
+    override fun toString() : String {
+        val clauses = mutableListOf("ID = $id")
+
+        if (parentLpObjective == Double.MAX_VALUE)
+            clauses.add("parentLp = $parentLpObjective")
+        else
+            clauses.add("parentLp = %.2f".format(parentLpObjective))
+
+        if (!lpFeasible)
+            clauses.add("infeasible")
+
+        return clauses.joinToString(", ", "Node(",")")
+    }
+
+}
